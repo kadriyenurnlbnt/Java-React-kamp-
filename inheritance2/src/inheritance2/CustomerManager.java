0bf4f3131dffcb706701a1356171914d89aee988
Kadriye Nur <65511863+kadriyenurnlbnt@users.noreply.github.com>
2021-04-30 16:10:04 +0300

    inheritance 2

---
+++ inheritance2/src/inheritance2/CustomerManager.java
@@ -0,0 +1,12 @@
+package inheritance2;
+
+public class CustomerManager {
+	public void add(Logger logger) {
+		
+		//müsteri ekleme kodlarý
+		System.out.println("müsteri eklendi");
+		
+		logger.log();
+	}
+
+}
